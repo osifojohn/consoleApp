f1841880114ea2db907f1983cc70cbf8f6bb7004
Osifo John <osifojohntec@gmail.com>
2022-12-08 23:05:16 +0100

    first commit

---
+++ dist/index.js
@@ -0,0 +1,59 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const prompt_sync_1 = __importDefault(require("prompt-sync"));
+let input = (0, prompt_sync_1.default)();
+let userInput = input("Enter keyword> ");
+const commandInstructions = `Press "1" to save book by name, "2" to retrieve book by name`;
+const handleUserInput = (userInput) => {
+    const listOfBooks = [];
+    const helper = (inputData) => {
+        if (inputData === "quit") {
+            return;
+        }
+        if (userInput !== "1" &&
+            userInput !== "Enter keyword> " &&
+            userInput !== "hints") {
+            const inValidCommand = (userInput) => {
+                console.log(`Unkown command "${userInput}": type 'hints' to see lists of supported commands`);
+            };
+            inValidCommand(inputData);
+            userInput = input("Please re-enter  keyword> ");
+            helper(userInput);
+        }
+        if (userInput === "hints") {
+            handleHints(userInput);
+        }
+        function handleHints(userInput) {
+            console.log(commandInstructions);
+            userInput = input("Please re-enter  keyword> ");
+            helper(userInput);
+        }
+        if (userInput === "1") {
+            addBooksToList(userInput);
+        }
+        function addBooksToList(userInput) {
+            const bookId = `${new Date().getMinutes()}${new Date().getSeconds()}`;
+            userInput = input("Enter book name and press enter> ");
+            if (userInput === "2") {
+                retrieveBooksFromList();
+            }
+            if (userInput !== "2") {
+                listOfBooks.push({ name: userInput, id: bookId });
+                helper(userInput);
+            }
+        }
+        if (userInput === "2") {
+            retrieveBooksFromList();
+        }
+        function retrieveBooksFromList() {
+            listOfBooks.length !== 0 && console.log(listOfBooks);
+            userInput = input("Enter  command> ");
+            helper(userInput);
+        }
+    };
+    helper(userInput);
+};
+handleUserInput(userInput);
---
+++ src/index.ts
@@ -0,0 +1,65 @@
+import prompt from "prompt-sync";
+
+let input = prompt();
+let userInput = input("Enter keyword> ");
+
+const commandInstructions = `Press "1" to save book by name, "2" to retrieve book by name`;
+
+const handleUserInput = (userInput: string) => {
+  const listOfBooks: { name: string; id: string }[] = [];
+
+  const helper = (inputData: string) => {
+    if (inputData === "quit") {
+      return;
+    }
+    if (
+      userInput !== "1" &&
+      userInput !== "Enter keyword> " &&
+      userInput !== "hints"
+    ) {
+      const inValidCommand = (userInput?: string) => {
+        console.log(
+          `Unkown command "${userInput}": type 'hints' to see lists of supported commands`
+        );
+      };
+      inValidCommand(inputData);
+      userInput = input("Please re-enter  keyword> ");
+      helper(userInput);
+    }
+
+    if (userInput === "hints") {
+      handleHints(userInput);
+    }
+    function handleHints(userInput: string) {
+      console.log(commandInstructions);
+      userInput = input("Please re-enter  keyword> ");
+      helper(userInput);
+    }
+
+    if (userInput === "1") {
+      addBooksToList(userInput);
+    }
+    function addBooksToList(userInput: string) {
+      const bookId = `${new Date().getMinutes()}${new Date().getSeconds()}`;
+      userInput = input("Enter book name and press enter> ");
+      if (userInput === "2") {
+        retrieveBooksFromList();
+      }
+      if (userInput !== "2") {
+        listOfBooks.push({ name: userInput, id: bookId });
+        helper(userInput);
+      }
+    }
+    if (userInput === "2") {
+      retrieveBooksFromList();
+    }
+    function retrieveBooksFromList() {
+      listOfBooks.length !== 0 && console.log(listOfBooks);
+      userInput = input("Enter  command> ");
+      helper(userInput);
+    }
+  };
+
+  helper(userInput);
+};
+handleUserInput(userInput);
